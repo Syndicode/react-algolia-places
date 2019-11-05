cc7bd92dbfc757b8b04bd5c6481ac141d1c6c3f4
Anton Rusinov <rusinovantondev@gmail.com>
2019-11-05 18:19:45 +0200

    apply babel only for umd bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 const peerDepsExternal = require("rollup-plugin-peer-deps-external");
 const babel = require("rollup-plugin-babel");
 
-module.exports = {
-  input: "./src/index.js",
-  output: [
-    {
+module.exports = [
+  {
+    input: "./src/index.js",
+    output: {
       file: "./build/bundle.umd.js",
       format: "umd",
       name: "ReactAlgoliaPlaces",
@@ -13,20 +13,26 @@ module.exports = {
         algoliasearch: "algoliasearch",
       },
     },
-    {
-      file: "./build/bundle.es.js",
-      format: "esm",
-    },
-    {
-      file: "./build/bundle.cjs.js",
-      format: "cjs",
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    babel({
-      exclude: "node_modules/**",
-      presets: ["@babel/env"],
-    }),
-  ],
-};
+    plugins: [
+      peerDepsExternal(),
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/env"],
+      }),
+    ],
+  },
+  {
+    input: "./src/index.js",
+    output: [
+      {
+        file: "./build/bundle.es.js",
+        format: "esm",
+      },
+      {
+        file: "./build/bundle.cjs.js",
+        format: "cjs",
+      },
+    ],
+    plugins: [peerDepsExternal()],
+  },
+];
